a188a9af0b3233036967c018479e00ba4480bd09
Julio Cesar Bolaños Palacios <cesar.bolanos@amk-technologies.com>
2018-03-20 21:30:45 -0600

    Add first class.

---
+++ src/main/java/mx/ninja/dev/App.java
@@ -0,0 +1,19 @@
+/*
+ *      File: App.java
+ *    Author: Julio Bolanos <cesar.bolanos@amk-technologies.com>
+ *      Date: mar 20, 2018
+ * Copyright: AMK Technologies, S.A. de C.V. 2018
+ */
+package mx.ninja.dev;
+
+/**
+ * Main class of the project.
+ * @author Julio Bolanos &lt;cesar.bolanos@amk-technologies.com&gt;
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java 8.");
+    }
+}
